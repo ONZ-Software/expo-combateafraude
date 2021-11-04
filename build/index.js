e7e4a39420fcdd0fc68ba4c4db5876d909e6a5cf
Thales Ogliari <thalesog@me.com>
2021-11-04 16:22:32 -0300

    chore: deploy

---
+++ build/index.js
@@ -0,0 +1,76 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const config_plugins_1 = require("@expo/config-plugins");
+const config_plugins_2 = require("@expo/config-plugins");
+const generateCode_1 = require("@expo/config-plugins/build/utils/generateCode");
+const fs_extra_1 = __importDefault(require("fs-extra"));
+const path_1 = __importDefault(require("path"));
+const withCombateAFraude = (config) => {
+    return (0, config_plugins_2.withDangerousMod)(config, [
+        'ios',
+        async (config) => {
+            const filePath = path_1.default.join(config.modRequest.platformProjectRoot, 'Podfile');
+            const contents = await fs_extra_1.default.readFile(filePath, 'utf-8');
+            let results = {
+                contents,
+            };
+            try {
+                results = addExternalPod(results.contents, `'DocumentDetector', '~> 4.8.3'`);
+                results = addExternalPod(results.contents, `'PassiveFaceLiveness', '~> 3.7.2'`);
+                results = addExternalPod(results.contents, `'FaceAuthenticator', '~> 2.5.0'`);
+                results = (0, generateCode_1.mergeContents)({
+                    tag: 'useFrameworks',
+                    src: results.contents,
+                    newSrc: `use_frameworks!\n`,
+                    anchor: /platform :ios/,
+                    offset: 1,
+                    comment: '#',
+                });
+                results.contents =
+                    results.contents +
+                        "source 'https://github.com/combateafraude/iOS.git'\n" +
+                        "source 'https://cdn.cocoapods.org/'\n";
+            }
+            catch (error) {
+                if (error.code === 'ERR_NO_MATCH') {
+                    throw new Error(`Cannot add Combate a Fraude to the project's ios/Podfile because it's malformed. Please report this with a copy of your project Podfile.`);
+                }
+                throw error;
+            }
+            if (results.didMerge || results.didClear) {
+                await fs_extra_1.default.writeFile(filePath, results.contents);
+            }
+            return config;
+        },
+    ]);
+};
+function addExternalPod(src, podName) {
+    return (0, generateCode_1.mergeContents)({
+        tag: 'ExternalPod' + String(Math.random()).substring(-3),
+        src,
+        newSrc: `  pod ${podName}`,
+        anchor: /use_react_native!/,
+        offset: 0,
+        comment: '#',
+    });
+}
+const withCafFiles = (config) => {
+    return (0, config_plugins_1.withXcodeProject)(config, async (cfg) => {
+        await fs_extra_1.default.copyFile(path_1.default.resolve(__dirname, './caf/CombateAFraude.m'), cfg.modRequest.platformProjectRoot + '/CombateAFraude.m');
+        await fs_extra_1.default.copyFile(path_1.default.resolve(__dirname, './caf/CombateAFraude.swift'), cfg.modRequest.platformProjectRoot + '/CombateAFraude.swift');
+        await fs_extra_1.default.copyFile(path_1.default.resolve(__dirname, './caf/Bridging-Header.h'), cfg.modRequest.platformProjectRoot + `/CombateAFraude-Bridging-Header.h`);
+        const pbxGroup = cfg.modResults.hash.project.objects.PBXGroup;
+        cfg.modResults.addFile(`CombateAFraude-Bridging-Header.h`, Object.keys(pbxGroup)[0]);
+        cfg.modResults.addFile('CombateAFraude.m', Object.keys(pbxGroup)[0]);
+        cfg.modResults.addFile('CombateAFraude.swift', Object.keys(pbxGroup)[0]);
+        return cfg;
+    });
+};
+const mainPlugin = (config) => (0, config_plugins_1.withPlugins)(config, [
+    [withCombateAFraude, {}],
+    [withCafFiles, {}],
+]);
+exports.default = mainPlugin;

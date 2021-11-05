1ae583c213dfc94e0778470e2fd938796eaa9757
Thales Ogliari <thalesog@me.com>
2021-11-05 20:52:04 -0300

    First Working Version (#1)
    * fix: change briding header
    * refactor: replace  methods with expo helpers
    * feat: android version
    * feat: add android caf source
    * feat: rename package on sources
    * feat: simplify code
    * feat: changes in main app replace method
    * chore: dotfiles and final touches

---
+++ build/index.d.ts
@@ -0,0 +1,3 @@
+import { ConfigPlugin } from '@expo/config-plugins';
+declare const mainPlugin: ConfigPlugin<void>;
+export default mainPlugin;
--- src/index.ts
+++ src/index.ts
@@ -1,110 +1,258 @@
 import {
   ConfigPlugin,
+  withAppBuildGradle,
   withPlugins,
+  withProjectBuildGradle,
   withXcodeProject,
 } from '@expo/config-plugins'
 import { withDangerousMod } from '@expo/config-plugins'
+import { getSourceRoot } from '@expo/config-plugins/build/ios/Paths'
+import {
+  addBuildSourceFileToGroup,
+  getProjectName,
+} from '@expo/config-plugins/build/ios/utils/Xcodeproj'
 import { mergeContents } from '@expo/config-plugins/build/utils/generateCode'
 import fs from 'fs-extra'
 import path from 'path'
 
-const withCombateAFraude: ConfigPlugin<void> = (config) => {
-  return withDangerousMod(config, [
-    'ios',
-    async (config) => {
-      const filePath = path.join(
-        config.modRequest.platformProjectRoot,
-        'Podfile'
+const withCafIos: ConfigPlugin<void> = (config) => {
+  const withXcodeFiles: ConfigPlugin<void> = (config) =>
+    withXcodeProject(config, async (cfg) => {
+      const srcRoot = getSourceRoot(cfg.modRequest.projectRoot)
+      const projName = getProjectName(cfg.modRequest.projectRoot)
+
+      // Copy CombateAFraude Source Files
+      await fs.copyFile(
+        path.resolve(__dirname, './caf/ios/CombateAFraude.m'),
+        cfg.modRequest.platformProjectRoot + '/CombateAFraude.m'
       )
-      const contents = await fs.readFile(filePath, 'utf-8')
-      let results: any = {
-        contents,
-      }
-      try {
-        results = addExternalPod(
-          results.contents,
-          `'DocumentDetector', '~> 4.8.3'`
+      await fs.copyFile(
+        path.resolve(__dirname, './caf/ios/CombateAFraude.swift'),
+        cfg.modRequest.platformProjectRoot + '/CombateAFraude.swift'
+      )
+
+      // Replace Main Briding-Header
+      await fs.copyFile(
+        path.resolve(__dirname, './caf/ios/Bridging-Header.h'),
+        srcRoot + `/${projName}-Bridging-Header.h`
+      )
+
+      cfg.modResults = addBuildSourceFileToGroup({
+        filepath: cfg.modRequest.platformProjectRoot + '/CombateAFraude.swift',
+        groupName: projName,
+        project: cfg.modResults,
+      })
+
+      cfg.modResults = addBuildSourceFileToGroup({
+        filepath: cfg.modRequest.platformProjectRoot + '/CombateAFraude.m',
+        groupName: projName,
+        project: cfg.modResults,
+      })
+
+      return cfg
+    })
+
+  const withPods: ConfigPlugin<void> = (config) =>
+    withDangerousMod(config, [
+      'ios',
+      async (config) => {
+        const filePath = path.join(
+          config.modRequest.platformProjectRoot,
+          'Podfile'
         )
-        results = addExternalPod(
-          results.contents,
-          `'PassiveFaceLiveness', '~> 3.7.2'`
-        )
-        results = addExternalPod(
-          results.contents,
-          `'FaceAuthenticator', '~> 2.5.0'`
-        )
-        results = mergeContents({
-          tag: 'useFrameworks',
-          src: results.contents,
-          newSrc: `use_frameworks!\n`,
-          anchor: /platform :ios/,
-          offset: 1,
-          comment: '#',
-        })
-        results.contents =
-          results.contents +
-          "source 'https://github.com/combateafraude/iOS.git'\n" +
-          "source 'https://cdn.cocoapods.org/'\n"
-      } catch (error: any) {
-        if (error.code === 'ERR_NO_MATCH') {
-          throw new Error(
-            `Cannot add Combate a Fraude to the project's ios/Podfile because it's malformed. Please report this with a copy of your project Podfile.`
-          )
+        const contents = await fs.readFile(filePath, 'utf-8')
+        let results: any = {
+          contents,
         }
-        throw error
-      }
+        try {
+          results = mergeContents({
+            tag: 'DocumentDetector',
+            src: results.contents,
+            newSrc: `  pod 'DocumentDetector'`,
+            anchor: /use_react_native!/,
+            offset: 0,
+            comment: '#',
+          })
+          results = mergeContents({
+            tag: 'PassiveFaceLiveness',
+            src: results.contents,
+            newSrc: `  pod 'PassiveFaceLiveness'`,
+            anchor: /use_react_native!/,
+            offset: 0,
+            comment: '#',
+          })
+          results = mergeContents({
+            tag: 'FaceAuthenticator',
+            src: results.contents,
+            newSrc: `  pod 'FaceAuthenticator'`,
+            anchor: /use_react_native!/,
+            offset: 0,
+            comment: '#',
+          })
+          results = mergeContents({
+            tag: 'useFrameworks',
+            src: results.contents,
+            newSrc: `use_frameworks!\n`,
+            anchor: /platform :ios/,
+            offset: 1,
+            comment: '#',
+          })
+          results.contents = `${results.contents}
+source 'https://github.com/combateafraude/iOS.git'
+source 'https://cdn.cocoapods.org/'
+            `
+        } catch (error: any) {
+          if (error.code === 'ERR_NO_MATCH') {
+            throw new Error(
+              `Cannot add Combate a Fraude to the project's ios/Podfile because it's malformed. Please report this with a copy of your project Podfile.`
+            )
+          }
+          throw error
+        }
 
-      if (results.didMerge || results.didClear) {
-        await fs.writeFile(filePath, results.contents)
-      }
+        if (results.didMerge || results.didClear) {
+          await fs.writeFile(filePath, results.contents)
+        }
 
-      return config
-    },
-  ])
+        return config
+      },
+    ])
+
+  return withPlugins(config, [withXcodeFiles, withPods])
 }
 
-function addExternalPod(src: string, podName: string) {
-  return mergeContents({
-    tag: 'ExternalPod' + String(Math.random()).substring(-3),
-    src,
-    newSrc: `  pod ${podName}`,
-    anchor: /use_react_native!/,
-    offset: 0,
-    comment: '#',
-  })
-}
+const withCafAndroid: ConfigPlugin<void> = (config) => {
+  const withCafSource: ConfigPlugin<void> = (expoCfg) => {
+    return withDangerousMod(expoCfg, [
+      'android',
+      async (config) => {
+        if (!config.android?.package) throw new Error('Missing package name')
+        await fs.copyFile(
+          path.resolve(__dirname, './caf/android/CombateAFraudeModule.java'),
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudeModule.java'
+        )
+        const moduleContents = await fs.readFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudeModule.java'
+        )
 
-const withCafFiles: ConfigPlugin = (config) => {
-  return withXcodeProject(config, async (cfg) => {
-    await fs.copyFile(
-      path.resolve(__dirname, './caf/CombateAFraude.m'),
-      cfg.modRequest.platformProjectRoot + '/CombateAFraude.m'
-    )
-    await fs.copyFile(
-      path.resolve(__dirname, './caf/CombateAFraude.swift'),
-      cfg.modRequest.platformProjectRoot + '/CombateAFraude.swift'
-    )
-    await fs.copyFile(
-      path.resolve(__dirname, './caf/Bridging-Header.h'),
-      cfg.modRequest.platformProjectRoot + `/CombateAFraude-Bridging-Header.h`
-    )
+        await fs.writeFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudeModule.java',
+          moduleContents
+            .toString()
+            .replace(/\[\[PACKAGE\]\]/, config.android?.package)
+        )
+        await fs.copyFile(
+          path.resolve(__dirname, './caf/android/CombateAFraudePackage.java'),
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudePackage.java'
+        )
+        const packageContents = await fs.readFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudePackage.java'
+        )
+        await fs.writeFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/CombateAFraudePackage.java',
+          packageContents
+            .toString()
+            .replace(/\[\[PACKAGE\]\]/, config.android?.package)
+        )
 
-    const pbxGroup = cfg.modResults.hash.project.objects.PBXGroup
+        // WORKARROUND ABOUT FAILED withMainApplication ON BUILD
+        const mainAppContents = await fs.readFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/MainApplication.java'
+        )
+        const mainAppContentsUpdated = mergeContents({
+          tag: 'Add Package',
+          src: mainAppContents.toString(),
+          newSrc: `      packages.add(new CombateAFraudePackage());`,
+          anchor: /return packages/,
+          offset: 0,
+          comment: '//',
+        }).contents
+        await fs.writeFile(
+          config.modRequest.platformProjectRoot +
+            '/app/src/main/java/br/com/b4u/MainApplication.java',
+          mainAppContentsUpdated
+        )
 
-    cfg.modResults.addFile(
-      `CombateAFraude-Bridging-Header.h`,
-      Object.keys(pbxGroup)[0]
-    )
-    cfg.modResults.addFile('CombateAFraude.m', Object.keys(pbxGroup)[0])
-    cfg.modResults.addFile('CombateAFraude.swift', Object.keys(pbxGroup)[0])
-    return cfg
-  })
+        return config
+      },
+    ])
+  }
+
+  const withFileMods: ConfigPlugin<void> = (config) => {
+    // DISABLED DUE FAIL ON BUILD -- NEEDS REVIEW
+    // const mainActivity: ConfigPlugin<void> = (expoCfg) =>
+    //   withMainApplication(expoCfg, async (config) => {
+    //     config.modResults.contents = mergeContents({
+    //       tag: 'Add Package',
+    //       src: config.modResults.contents,
+    //       newSrc: `      packages.add(new CombateAFraudePackage());`,
+    //       anchor: /return packages/,
+    //       offset: 0,
+    //       comment: '//',
+    //     }).contents
+    //     return config
+    //   })
+    const projectBuild: ConfigPlugin<void> = (expoCfg) =>
+      withProjectBuildGradle(expoCfg, async (config) => {
+        config.modResults.contents = mergeContents({
+          tag: 'Maven Repo',
+          src: config.modResults.contents,
+          newSrc: `        maven { url "https://repo.combateafraude.com/android/release" }`,
+          anchor: /mavenLocal\(\)/,
+          offset: 1,
+          comment: '//',
+        }).contents
+        // console.log('PROJECT BUILD GRADLE => ', mainApplication)
+        return config
+      })
+    const appBuild: ConfigPlugin<void> = (expoCfg) =>
+      withAppBuildGradle(expoCfg, async (config) => {
+        config.modResults.contents = mergeContents({
+          tag: 'Android Config',
+          src: config.modResults.contents,
+          newSrc: `
+  aaptOptions {
+    noCompress "tflite"
+  }
+
+  dataBinding {
+    enabled = true
+  }`,
+          anchor: /android {/,
+          offset: 1,
+          comment: '//',
+        }).contents
+
+        config.modResults.contents = mergeContents({
+          tag: 'Dependencies',
+          src: config.modResults.contents,
+          newSrc: `
+    implementation "com.combateafraude.sdk:passive-face-liveness:+"
+    implementation "com.combateafraude.sdk:document-detector:+"
+    implementation "com.combateafraude.sdk:face-authenticator:+"
+          `,
+          anchor: /dependencies {/,
+          offset: 1,
+          comment: '//',
+        }).contents
+
+        return config
+      })
+
+    return withPlugins(config, [projectBuild, appBuild])
+  }
+
+  return withPlugins(config, [withCafSource, withFileMods])
 }
 
 const mainPlugin: ConfigPlugin<void> = (config) =>
-  withPlugins(config, [
-    [withCombateAFraude, {}],
-    [withCafFiles, {}],
-  ])
+  withPlugins(config, [withCafIos, withCafAndroid])
 
 export default mainPlugin

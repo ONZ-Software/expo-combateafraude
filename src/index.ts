f5630a0e27fe11bdc85f13f8231daf1d1d4f7a81
cristiankrein <cristian.krein@outlook.com>
2024-09-19 14:47:38 -0300

    fix: face liveness sdk update

--- src/index.ts
+++ src/index.ts
@@ -348,7 +348,7 @@ const withCafAndroid: ConfigPlugin<void> = (config) => {
           implementation 'com.combateafraude.sdk:document-detector:8.1.5'
           implementation 'com.combateafraude.sdk:passive-face-liveness:6.0.0-rc07'
           implementation 'com.combateafraude.sdk:face-authenticator:5.8.14'
-          implementation 'com.combateafraude.sdk:new-face-liveness:3.1.0'
+          implementation 'com.combateafraude.sdk:new-face-liveness:3.2.0'
           `,
           anchor: /dependencies {/,
           offset: 1,
